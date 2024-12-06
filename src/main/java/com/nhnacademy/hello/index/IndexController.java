985bcabadf5014680bb564e03bf4cb81e66de9dd
jmg5642 <1>
2024-12-06 13:42:45 +0900

    hi

--- src/main/java/com/nhnacademy/hello/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/index/IndexController.java
@@ -14,4 +14,9 @@ public class IndexController {
     public String test(){
         return "index/test";
     }
+
+    @GetMapping("/payment")
+    public String payment(){
+        return "index/payment";
+    }
 }
